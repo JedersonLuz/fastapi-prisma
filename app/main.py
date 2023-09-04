8b4c2d200df2bc4abcd49a2d4330719baaa06e83
Jederson Sousa Luz <jedersonalpha@gmail.com>
2023-09-04 16:05:47 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,11 +5,17 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import posts, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
 
 
-app.include_router(users.router)
-app.include_router(posts.router)
+app.include_router(
+    users.router, 
+    dependencies=[Depends(get_query_token)]
+)
+app.include_router(
+    posts.router, 
+    dependencies=[Depends(get_query_token)]
+)
 app.include_router(
     admin.router,
     prefix="/admin",
